8381a02d648af0797bf2d7899f05914db5f5b8b7
Banoona <bayulistio25@gmail.com>
2022-02-16 23:39:47 +0700

    first commit

--- django_web/django_web/settings.py
+++ django_web/django_web/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)^6ss%p@6gg%v&1w7#1)z68spc)4d*a@n4h(e0_2x$vwa7)*f0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'byanscake.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'cakedemo.herokuapp.com',]
 
 STRIPE_SECRET_KEY = 'sk_test_51KS1h2FyszVY9qJpcjz9KXnfT8RnwynxAx5SBj4TTX4ZQguDdaOhXuNYEJOE9BAI8cLMbCPQO628PqzzVHa61HxL00ngpRZuGH'
 
@@ -135,16 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = BASE_DIR / 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
