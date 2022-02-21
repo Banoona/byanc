4e6839200cad5440f681fd8e655dd64a624e1556
Banoona <bayulistio25@gmail.com>
2022-02-21 23:54:42 +0700

    first commit

--- django_web/django_web/settings.py
+++ django_web/django_web/settings.py
@@ -140,13 +140,14 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
